cb02d33a38b69804ff589657ab128ceeb4fe6f53
Aditya Kumar Singh <adityakumar2809@gmail.com>
2019-02-22 02:52:03 +0530

    Project tab done with bundles

--- credentials/views.py
+++ credentials/views.py
@@ -7,24 +7,25 @@ def index(request):
 	candidate = models.Resume.objects.get(id__iexact = 1)
 	qualifications = models.Qualification.objects.all().filter(candidate__id__iexact = 1)
 	projects = models.Project.objects.all().filter(candidate__id__iexact = 1)
+	projects_list=[]
+	for pro in projects:
+		projects_dict={}
+		projects_dict['name']=pro.name
+		projects_dict['link']=pro.link
+		projects_dict['description']=pro.description
+		projects_dict['image']=pro.image.url
+		projects_list.append(projects_dict)
+
+
 	expertise = models.Expertise.objects.all().filter(candidate__id__iexact = 1)
 
-
-
-	# qualifications_dict={}
-	# for qual in qualifications:
-	# 	qualifications_dict[qual.name] = qual.link
-
-	# projects_dict={}
-	# for pro in projects:
-	# 	projects_dict[pro.name] = pro.link
-
-	# expertise_list = []
-	# for exp in expertise:
-	# 	expertise_list.append(exp.message)
-
-
-	
-	# return render(request, 'index.html', {'candidate_name':candidate.name, 'candidate_about':candidate.about, 'candidate_contact':candidate.contact, 'qualifications_dict':qualifications_dict, 'projects_dict':projects_dict, 'expertise_list':expertise_list, 'projects':projects })
-	return render(request, 'index.html', {'candidate':candidate,  'qualifications':qualifications, 'expertise':expertise, 'projects':projects })
+	return render(request, 'index.html', {	'candidate':candidate,  
+											'qualifications':qualifications, 
+											'expertise':expertise, 
+											'projects':projects, 
+											'projects_count':range((projects.count()//3)),
+											'projects_residue':range((projects.count()%3)),
+											'projects_list':projects_list, 
+											'three':range(3), 
+										})
 
