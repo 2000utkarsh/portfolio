f9130337e9339f40d2115707442eeaf55a990a67
Aditya Kumar Singh <adityakumar2809@gmail.com>
2019-02-24 16:05:40 +0530

    hasOpenClass

--- static/new/index.js
+++ static/new/index.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
 
+
+
 	function removeOpenClass(){
 		$('.all_tabs').each(function(i,el){
 			if ($(el).hasClass('open')) {
 				$(el).removeClass('open');
+			}
+		});
+	}
+
+	function hasOpenClass(){
+		$('.all_tabs').each(function(i,el){
+			if ($(el).hasClass('open')) {
+
+				return 1;
+			}
+			else{
+				return 0;
 			}
 		});
 	}
@@ -16,7 +30,8 @@ $(document).ready(function(){
 	$('.js-open-main_tab_1').on('click',function(e){
 		e.preventDefault();
 		removeOpenClass();
-		$('.main_tab_1').addClass('open')
+		$('.main_tab_1').addClass('open');		
+		$('.main_tab_1').css("display","block");		
 		$('.tab_link_single').addClass('trans_close');
 		$('.perm_header_center').addClass('trans_open');
 	});
@@ -24,7 +39,7 @@ $(document).ready(function(){
 	$('.js-open-main_tab_2').on('click',function(e){
 		e.preventDefault();
 		removeOpenClass();
-		$('.main_tab_2').addClass('open')
+		$('.main_tab_2').addClass('open');
 		$('.tab_link_single').addClass('trans_close');
 		$('.perm_header_center').addClass('trans_open');
 	});
@@ -32,7 +47,7 @@ $(document).ready(function(){
 	$('.js-open-main_tab_3').on('click',function(e){
 		e.preventDefault();
 		removeOpenClass();
-		$('.main_tab_3').addClass('open')
+		$('.main_tab_3').addClass('open');
 		$('.tab_link_single').addClass('trans_close');
 		$('.perm_header_center').addClass('trans_open');
 	});
@@ -40,8 +55,19 @@ $(document).ready(function(){
 	$('.js-open-main_tab_4').on('click',function(e){
 		e.preventDefault();
 		removeOpenClass();
-		$('.main_tab_4').addClass('open')
+		$('.main_tab_4').addClass('open');
 		$('.tab_link_single').addClass('trans_close');
 		$('.perm_header_center').addClass('trans_open');
 	});
+
+
+	$(window).on('scroll',function(){
+		console.log(hasOpenClass())
+	    if(($(window).scrollTop()) > (792-($(window).innerHeight())) && !(hasOpenClass())){
+	    	$('.all_tabs').css("display","none");
+	    	$(document.body).css("overflow","hidden");
+	    }
+  });
+
+
 });
