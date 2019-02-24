431277c9009bb05c38b586270ae787382c96e678
Aditya Kumar Singh <adityakumar2809@gmail.com>
2019-02-24 18:15:43 +0530

    fully functional transitions

--- static/new/index.js
+++ static/new/index.js
@@ -10,16 +10,29 @@ $(document).ready(function(){
 		});
 	}
 
-	function hasOpenClass(){
-		$('.all_tabs').each(function(i,el){
-			if ($(el).hasClass('open')) {
+	// function hasOpenClass(){
+	// 	$('.all_tabs').each(function(i,el){
+	// 		if ($(el).hasClass('open')) {
 
-				return 1;
-			}
-			else{
-				return 0;
-			}
-		});
+	// 			return 1;
+	// 		}
+	// 		else{
+	// 			return 0;
+	// 		}
+	// 	});
+	// }
+
+	function hasOpenClass() {
+		var tabs = document.querySelectorAll('.all_tabs');
+		var tabsArr = Array.prototype.slice.call(tabs);
+		 for(i = 0 ; i < tabsArr.length ; i++) {
+		 	if (tabsArr[i].classList.contains('open')) {
+		 		return i+1;
+		 	}
+
+		 }
+		 return 0;
+		
 	}
 
 	$('.js-close').on('click', function(e){
@@ -30,8 +43,8 @@ $(document).ready(function(){
 	$('.js-open-main_tab_1').on('click',function(e){
 		e.preventDefault();
 		removeOpenClass();
-		$('.main_tab_1').addClass('open');		
-		$('.main_tab_1').css("display","block");		
+		$('.main_tab_1').css("display","block");
+		setTimeout(function(){ $('.main_tab_1').addClass('open'); }, 500);		
 		$('.tab_link_single').addClass('trans_close');
 		$('.perm_header_center').addClass('trans_open');
 	});
@@ -39,7 +52,8 @@ $(document).ready(function(){
 	$('.js-open-main_tab_2').on('click',function(e){
 		e.preventDefault();
 		removeOpenClass();
-		$('.main_tab_2').addClass('open');
+		$('.main_tab_2').css("display","block");
+		setTimeout(function(){ $('.main_tab_2').addClass('open'); }, 500);
 		$('.tab_link_single').addClass('trans_close');
 		$('.perm_header_center').addClass('trans_open');
 	});
@@ -47,7 +61,8 @@ $(document).ready(function(){
 	$('.js-open-main_tab_3').on('click',function(e){
 		e.preventDefault();
 		removeOpenClass();
-		$('.main_tab_3').addClass('open');
+		$('.main_tab_3').css("display","block");		
+		setTimeout(function(){ $('.main_tab_3').addClass('open'); }, 500);
 		$('.tab_link_single').addClass('trans_close');
 		$('.perm_header_center').addClass('trans_open');
 	});
@@ -55,18 +70,48 @@ $(document).ready(function(){
 	$('.js-open-main_tab_4').on('click',function(e){
 		e.preventDefault();
 		removeOpenClass();
-		$('.main_tab_4').addClass('open');
+		$('.main_tab_4').css("display","block");
+		setTimeout(function(){ $('.main_tab_4').addClass('open'); }, 500);
 		$('.tab_link_single').addClass('trans_close');
 		$('.perm_header_center').addClass('trans_open');
 	});
 
 
 	$(window).on('scroll',function(){
-		console.log(hasOpenClass())
-	    if(($(window).scrollTop()) > (792-($(window).innerHeight())) && !(hasOpenClass())){
-	    	$('.all_tabs').css("display","none");
-	    	$(document.body).css("overflow","hidden");
-	    }
+		if(hasOpenClass() === 0){
+		    if(($(window).scrollTop()) > (($('.main').height())-($(window).innerHeight()))){
+		    	$('.all_tabs').css("display","none");
+		    	$(document.body).css("overflow","hidden");
+		    }
+		}
+		else if(hasOpenClass() === 1){
+			if(($(window).scrollTop()) > (($('.main_tab_1').height())-($(window).innerHeight()))){
+				$('.main_tab_1').css("display","block");
+		    	$('.main_tab_2, .main_tab_3, .main_tab_4').css("display","none");
+		    	$(document.body).css("overflow","hidden");
+		    }
+		}
+		else if(hasOpenClass() === 2){
+			if(($(window).scrollTop()) > (($('.main_tab_2').height())-($(window).innerHeight()))){
+				$('.main_tab_2').css("display","block");
+		    	$('.main_tab_1, .main_tab_3, .main_tab_4').css("display","none");
+		    	$(document.body).css("overflow","hidden");
+		    }
+		}
+		else if(hasOpenClass() === 3){
+			if(($(window).scrollTop()) > (($('.main_tab_3').height())-($(window).innerHeight()))){
+				$('.main_tab_3').css("display","block");
+		    	$('.main_tab_1, .main_tab_2, .main_tab_4').css("display","none");
+		    	$(document.body).css("overflow","hidden");
+		    }
+		}
+		else if(hasOpenClass() === 4){
+			if(($(window).scrollTop()) > (($('.main_tab_4').height())-($(window).innerHeight()))){
+				$('.main_tab_4').css("display","block");
+		    	$('.main_tab_1, .main_tab_2, .main_tab_3').css("display","none");
+		    	$(document.body).css("overflow","hidden");
+		    }
+		}
   });
 
 
